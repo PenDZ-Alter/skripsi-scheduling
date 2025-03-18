05ade0d75c2e90e9bab86f6bd68bacf4379ecd0a
PenDZ-Alter <abxpendz45@gmail.com>
2025-03-18 21:06:15 +0700

    fix: dynamic margin

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,8 +9,9 @@
   <link href="storage/img/UIN-2.1.png" rel='shortcut icon'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
+
 <body class="bg-gray-100 text-gray-800">
-    <!-- Kontainer utama, min-h-screen agar tinggi minimal = tinggi layar -->
+  <!-- Kontainer utama, min-h-screen agar tinggi minimal = tinggi layar -->
   <div class="min-h-screen flex pl-64">
     <!-- SIDEBAR -->
     <aside class="fixed top-0 left-0 bg-indigo-900 text-white w-64 h-screen overflow-y-auto z-50">
@@ -19,7 +21,27 @@
 
     <!-- BAGIAN KANAN: HEADER + MAIN + FOOTER -->
     <!-- flex-1 agar kolom kanan memenuhi sisa ruang -->
-    <div class="flex-1 flex flex-col ml-64">
+    <div class="flex-1 flex flex-col ml-32 ml-64 dynamic-margin">
+      <script>
+        function adjustMargin() {
+          const contentDiv = document.querySelector(".dynamic-margin");
+          if (window.innerWidth >= 1920) {
+            contentDiv.classList.remove("ml-64");
+            contentDiv.classList.add("ml-32");
+          } else {
+            contentDiv.classList.remove("ml-32");
+            contentDiv.classList.add("ml-64");
+          }
+        }
+
+        console.log(`DEBUG :: ${window.innerWidth}`);
+
+        // Panggil saat pertama kali halaman dimuat
+        adjustMargin();
+
+        // Panggil ulang saat ukuran layar berubah
+        window.addEventListener("resize", adjustMargin);
+      </script>
       <!-- HEADER -->
       @include('partials.header')
 
@@ -33,4 +55,5 @@
     </div>
   </div>
 </body>
+
 </html>
