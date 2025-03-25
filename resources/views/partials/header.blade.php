aec34b1242d1ac0e524ce7413a4709b6a5c3b03d
Ditto-Prasetyo <dittoprasetyo100@gmail.com>
2025-03-25 16:08:34 +0700

    fix: fixing header texture

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
-<header style="background-color: #627a98" class="shadow p-4 flex justify-between items-center">
+<header style="background-color: #627a98; height:3rem;" class="shadow p-4 flex justify-between items-center">
     <div class="flex items-center space-x-1">
-        <h1 style="font-size: 1.25rem; color: white; font-weight: 300; letter-spacing: 2px;">Salam,</h1>
-        <p style="font-size: 1rem; color: white; font-weight: 500; position: relative; top: 2px; letter-spacing: 2px;">ALFARIZ MUHAN MANDEGA</p>
+        <h1 style="font-size: 1.3rem; color: white; font-weight: 300; letter-spacing: 2px;">Salam,</h1>
+        <p style="font-size: 0.9rem; color: white; font-weight: 500; position: relative; top: 3px; left: 3px; letter-spacing: 2px;">ALFARIZ MUHAN MANDEGA</p>
     </div>
 
     <div class="flex items-center space-x-4">
