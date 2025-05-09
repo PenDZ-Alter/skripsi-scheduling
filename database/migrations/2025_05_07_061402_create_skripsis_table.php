22be27cda3b414ca8c258960cc4f6eee9b5b63c9
PenDZ-Alter <abxpendz45@gmail.com>
2025-05-09 14:03:02 +0700

    feat(db): adding `ruang_sidang` and `skripsis`

--- database/migrations/2025_05_07_061402_create_skripsis_table.php
+++ database/migrations/2025_05_07_061402_create_skripsis_table.php
@@ -14,14 +14,18 @@ return new class extends Migration
         Schema::create('skripsis', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id'); // mahasiswa yang punya skripsi
+            $table->unsignedBigInteger('ruang_sidang');
             $table->string('judul');
-            $table->string('dosen_pembimbing');
+            $table->string('dosen_pembimbing_1');
+            $table->string('dosen_pembimbing_2');
+            $table->string('dosen_pembimbing_3');
             $table->date('jadwal_sidang')->nullable();
             $table->enum('status', ['belum_sidang', 'lulus', 'revisi'])->default('belum_sidang');
             $table->timestamps();
     
             // Relasi ke tabel users
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('ruang_sidang')->references('id')->on('ruang_sidangs')->onDelete('cascade');
         });
     }
 
