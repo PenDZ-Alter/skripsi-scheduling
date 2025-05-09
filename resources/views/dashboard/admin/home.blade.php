0bd298f6418ed2e2cf1ceb08e08b1f92c9f92b71
PenDZ-Alter <abxpendz45@gmail.com>
2025-05-09 16:16:12 +0700

    feat: add admin dashboard page

---
+++ resources/views/dashboard/admin/home.blade.php
@@ -0,0 +1,245 @@
+@extends('dashboard.admin.master')
+
+@section('content')
+    <!-- Grid 2 Kolom -->
+    <div class="profile-card">
+        <!-- Background image -->
+        <div class="absolute inset-0">
+            <img src="{{ asset('storage/img/Background.jpeg') }}" alt="Background"
+                class="grayscale background-Image">
+        </div>
+
+        <!-- Konten -->
+        <div class="grid-cols-1 md:grid-cols-2 content-Card">
+            <!-- Kolom Kiri: Info Profil -->
+            <div class="space-x-4 card-Profile">
+                <!-- Pembungkus foto dan icon -->
+                <div class="profile-wrapper">
+                    <!-- Foto profil -->
+                    <div class="profile-Image-background">
+                        <a href="{{ route('profile') }}" class="profile-Image-background1"><img class="min-w-1 min-h-0.5" id="profileImage" src="{{ asset('storage/img/Alfariz.png') }}"
+                            alt="Foto Profil" class="profile-image"></a>
+
+                        <!-- Ikon kamera -->
+                        <div class="camera-fix" onclick="document.getElementById('uploadInput').click();">
+                            <img src="{{ asset('storage/img/camera-fix.png') }}" alt="Camera Icon">
+                        </div>
+                    </div>
+
+
+                    <!-- Input file untuk unggah -->
+                    <form id="uploadForm" enctype="multipart/form-data">
+                        @csrf
+                        <input id="uploadInput" name="photo" type="file" class="hidden" accept="image/*"
+                            onchange="previewAndUploadImage()">
+                    </form>
+                </div>
+
+                <!-- Info Profile -->
+                <div class="info-profile">
+                    <h3 class="judul-text">Info Profil & Email UIN, Klik
+                        <a href="{{ url('/profileEdit') }}" class="link-profile"><u>Di SINI</u></a>
+                    </h3>
+                    <p class="text-pertama"> - Default password email sesuai siakad</p>
+                    <p class="text-kedua"> - Ubah password email, klik
+                        <a href="{{ url('/profileEdit') }}" class="link-ubahPassword">Di SINI</a>
+                    </p>
+                    <p class="text-ketiga"> - Buka email UIN, klik
+                        <a href="{{ url('/profileEdit') }}" class="link-Email">gmail.com</a>
+                    </p>
+                    <p class="text-keempat"> Add account:
+                        <i class="paragraf-Email">NIM</i>@student.uin-malang.ac.id
+                    </p>
+                    <p class="text-kelima"> - Untuk aktivasi email UIN, klik
+                        <a href="{{ url('/profileEdit') }}" class="link-aktivasiEmail">Di SINI</a>
+                    </p>
+                </div>
+            </div>
+
+            <!-- Kolom Kanan: Free Access Journal -->
+            <div>
+                <h3 class="title-Link">Free Access Journal</h3>
+                <div class="space-y-2">
+                    <a href="https://link.springer.com/" class="link-springer">
+                        Free Access Link Springer <span class="first-laquo">&laquo;</span>
+                    </a>
+                    <a href="https://www.emerald.com/insight/" class="link-emerald">
+                        Free Access Emerald Insight <span class="second-laquo">&laquo;</span>
+                    </a>
+                    <a href="https://www.cambridge.org/core/" class="link-cambridge">
+                        Free Access Cambridge Core <span class="third-laquo">&laquo;</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Grafik IP dan IPK -->
+    <div class="Card">
+        <div class="card-Chart">
+            <!-- 📊 Grafik Indeks Prestasi -->
+            <div id="grafikContainer" class="chart-IP">
+                <!-- Tombol Ekspor -->
+                <div class="top-2 right-2 dropdown-button">
+                    <div id="dropdownButton" class="dropdown-menu" onclick="toggleDropdown()">
+                        ☰
+                    </div>
+                    <ul id="exportMenu" class="hidden export-menu">
+                        <li><a href="#" id="downloadPNG" class="menu-item">Download PNG</a></li>
+                        <li><a href="#" id="downloadJPEG" class="menu-item">Download JPEG</a></li>
+                        <li><a href="#" id="downloadPDF" class="menu-item">Download PDF</a></li>
+                    </ul>
+                </div>
+
+                <!-- Judul -->
+                <h3 class="chart-IP-text">Grafik Indeks Prestasi</h3>
+                <p class="mahasiswa-Information">
+                    220605110025 - <span class="mahasiswa-Name">ALFARIZ MUHAN MANDEGA</span>
+                </p>
+                <canvas id="grafikIP" height="110" width="auto"></canvas>
+            </div>
+
+            <!-- 🖼️ Area Gambar dengan Swiper -->
+            <div class="card-Swiper">
+                <div class="swiper mySwiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <a href="https://hes.uin-malang.ac.id/wp-content/uploads/2022/12/Sertifikat-dan-SK-BAN-PT-Akreditasi-Prodi-HES-6-Desember-2022-sd-2027.pdf"
+                                target="_blank" class="Image1">
+                                <img src="{{ asset('storage/img/Berita1.jpeg') }}"
+                                    class="Image1-Style" />
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="https://id.quora.com/Apa-penyesalanmu-selama-kuliah" target="_blank" class="Image2">
+                                <img src="{{ asset('storage/img/Berita2.png') }}"
+                                    class="Image2-Style" />
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="https://docs.google.com/forms/d/e/1FAIpQLSf04cEpb6v-44beFfiPUwthljHggOcefmBtow0PyJu7uOwd3A/viewform?pli=1"
+                                target="_blank" class="Image3">
+                                <img src="{{ asset('storage/img/Berita3.png') }}"
+                                    class="Image3-Style" />
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="https://www.instagram.com/kemendukbangga_bkkbn/" target="_blank" class="Image4">
+                                <img src="{{ asset('storage/img/Berita4.jpg') }}"
+                                    class="Image4-Style" />
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="https://www.instagram.com/simfonifmmalang/p/C3PzBgYPnSK/" target="_blank"
+                                class="Image5">
+                                <img src="{{ asset('storage/img/Berita5.jpeg') }}"
+                                    class="Image5-Style" />
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="storage/img/Berita6.jpg" target="_blank"
+                                class="Image6">
+                                <img src="{{ asset('storage/img/Berita6.jpg') }}"
+                                    class="Image6-Style" />
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="https://library.uin-malang.ac.id/" target="_blank"
+                                class="Image7">
+                                <img src="{{ asset('storage/img/Berita7.jpg') }}"
+                                    class="Image7-Style" />
+                            </a>
+                        </div>
+                    </div>
+
+                    <!-- Tombol Navigasi -->
+                    <div style="color:#FFFFFF !important;" class="swiper-button-prev"></div>
+                    <div style="color:#FFFFFF !important;" class="swiper-button-next"></div>
+
+                    <!-- Pagination -->
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Grid 2 Kolom untuk Data Akademik & Jadwal Kuliah -->
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+        <!-- Card Data Akademik -->
+        <div class="student-info">
+            <div class="info-box">
+                <div class="space-y-2">
+                    <div class="row mb-2">
+                        <div class="col-3 label">
+                            <span class="font-semibold">NIM</span>
+                        </div>
+                        <div class="col-9 value border-bottom">
+                            220605110025
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-3 label">
+                            <span class="font-semibold">Status Studi</span>
+                        </div>
+                        <div class="col-9 value border-bottom d-flex align-items-center">
+                            <span class="status-indicator"></span> <span class="beside-status">Mahasiswa Aktif</span>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-3 label">
+                            <span class="font-semibold">Jurusan</span>
+                        </div>
+                        <div class="col-9 value border-bottom">
+                            S1 TEKNIK INFORMATIKA
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-3 label">
+                            <span class="font-semibold">Akreditasi</span>
+                        </div>
+                        <div class="col-9 value border-bottom">
+                            Unggul (040/SK/LAM-INFOKOM/Ak/S/III/2024)
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-3 label">
+                            <span class="font-semibold">Dosen Wali</span>
+                        </div>
+                        <div class="col-9 value border-bottom">
+                            Dr. Zainal Abidin M.Kom
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-3 label">
+                            <span class="font-semibold">Semester</span>
+                        </div>
+                        <div class="col-9 value border-bottom">
+                            VI (Enam)
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Card Jadwal Kuliah -->
+        <div class="calendar-card">
+            <h3 class="calendar-title">
+                <i class="fa-regular fa-calendar"></i> Jadwal Kuliahmu Hari Ini
+            </h3>
+            <p class="calendar-description">Tidak Ada Jadwal Kuliah, saatnya kamu eksplorasi pengalaman baru</p>
+        </div>
+    </div>
+
+    <!-- Quotes -->
+    <div class="background-quotes">
+        <blockquote class="quotes-description">"Lelah hadir untuk menjadi pengingat seberapa besar perjuanganmu terhadap negara. Kalau
+            kamu lelah, rehat sejenak."</blockquote>
+        <span class="quotes-end">🌄 Positive Quotes</span>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
+@endsection
--- resources/views/dashboard/admin/master.blade.php
+++ resources/views/dashboard/admin/master.blade.php
@@ -6,20 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>.:: Sistem Informasi Akademik Universitas</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="storage/css/admin/icon.css" rel="stylesheet">
     <link href="storage/img/UIN-2.1.png" rel='shortcut icon'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="storage/css/mahasiswa/header.css">
-    <link rel="stylesheet" href="storage/css/mahasiswa/sidebar.css">
-    <link rel="stylesheet" href="storage/css/mahasiswa/HomePage.css">
-    <link rel="stylesheet" href="storage/css/mahasiswa/profile_page.css">
-    <link rel="stylesheet" href="storage/css/mahasiswa/headerProfile.css">
-    <link rel="stylesheet" href="storage/css/mahasiswa/transkrip_page.css">
-    <link rel="stylesheet" href="storage/css/mahasiswa/skripsi_page.css">
-    <link rel="stylesheet" href="storage/css/mahasiswa/pembayaran_page.css">
-    <link rel="stylesheet" href="storage/css/mahasiswa/HasilStudiPage.css">
-    <link rel="stylesheet" href="storage/css/mahasiswa/studiPage.css">
+    <link rel="stylesheet" href="storage/css/admin/header.css">
+    <link rel="stylesheet" href="storage/css/admin/sidebar.css">
+    <link rel="stylesheet" href="storage/css/admin/HomePage.css">
 </head>
 
 
@@ -28,11 +22,11 @@
     <div class="min-h-screen pl-64 md:pl-64 sm:pl-0">
         <!-- SIDEBAR -->
         <aside class="fixed top-0 left-0 w-64 h-full bg-indigo-900 text-white z-50">
-            @include('dashboard.mahasiswa.partials.sidebar')
+            @include('dashboard.admin.partials.sidebar')
         </aside>
 
         <!-- HEADER -->
-        @include('dashboard.mahasiswa.partials.header')
+        @include('dashboard.admin.partials.header')
 
         <!-- MAIN CONTENT -->
         <main class="p-6">
@@ -40,7 +34,7 @@
         </main>
 
         <!-- FOOTER -->
-        @include('dashboard.mahasiswa.partials.footer')
+        @include('dashboard.admin.partials.footer')
     </div>
     <script type="text/javascript" src="storage/js/function.js"></script>
     <script type="text/javascript" src="storage/js/IconDownload.js"></script>
