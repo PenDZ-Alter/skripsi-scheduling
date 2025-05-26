0980421dbd35b72ce68f5f3859ae84d8ee3bfd35
PenDZ-Alter <abxpendz45@gmail.com>
2025-05-26 10:41:00 +0700

    fix(user): reconfigure controller for users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,19 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-        return view("dashboard.mahasiswa.home");
+        $user = auth()->user();
+
+        if (!$user) {
+            return redirect()->route('login'); // fallback klo belum login
+        }
+
+        switch ($user->role) {
+            case 'admin':
+                return redirect()->route('adm.home');
+            case 'dosen':
+                return redirect()->route('mhs.home');
+            default:
+                abort(403, 'Role tidak dikenali.');
+        }
     }
 }
