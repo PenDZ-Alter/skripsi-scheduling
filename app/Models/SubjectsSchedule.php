9dcc77515dff694f0ceacdf6ab50c2bde65b3120
PenDZ-Alter <abxpendz45@gmail.com>
2025-05-29 13:40:44 +0700

    fix(model): adding timestamps

--- app/Models/SubjectsSchedule.php
+++ app/Models/SubjectsSchedule.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SubjectsSchedule extends Model
 {
-    public $timestamps = false;
+    public $timestamps = true;
 
     protected $fillable = ['dosen', 'nama_matkul', 'jadwal_mulai', 'jadwal_selesai', 'kelas'];
 
