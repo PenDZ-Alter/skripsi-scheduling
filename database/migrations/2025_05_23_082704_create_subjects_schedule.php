bb6f5ffdf22752f9245ba051af0176fd2bbf8da6
PenDZ-Alter <abxpendz45@gmail.com>
2025-05-29 18:21:46 +0700

    fix(db): rename database

--- database/migrations/2025_05_23_082704_create_subjects_schedule.php
+++ database/migrations/2025_05_23_082704_create_subjects_schedule.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('subjects_schedule');
+        Schema::dropIfExists('subjects_schedules');
     }
 };
