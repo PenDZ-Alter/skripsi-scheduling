f315b25b8e753e005038be0d6ab4eafc99368858
Ditto-Prasetyo <dittoprasetyo100@gmail.com>
2025-06-07 15:57:29 +0700

    fix(auth): add the lost `provider` files, `routes` files and add data function on `adminController`

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Skripsi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 use Carbon\Carbon;
 
 class AdminController extends Controller
@@ -60,5 +61,17 @@ class AdminController extends Controller
                     });
             })
             ->exists();
+    }
+
+    public function showDataAdminSidebar(){
+        $adminData1 = auth()->user();
+
+        return view('dashboard.admin.partials.sidebar', compact('adminData'));
+    }
+
+    public function showDataAdminHeader(){
+        $adminData2 = auth()->user();
+
+        return view('dashboard.admin.partials.header', compact('adminData'));
     }
 }
