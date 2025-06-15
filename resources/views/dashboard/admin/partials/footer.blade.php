eecfd7577abddfb0e05f4aff6f8d9ce238f7d028
alfariz-mm <alfarizmm@gmail.com>
2025-06-15 19:03:16 +0700

    position footer text

--- resources/views/dashboard/admin/partials/footer.blade.php
+++ resources/views/dashboard/admin/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="p-4 shadow text-center footer">
-    <div class="container mx-auto bg-transparent">
+    <div class="container-footer mx-auto bg-transparent">
         <p class="text-sm text-gray-700 footer-text">
             &copy; {{ date('Y') }} Universitas Islam Negeri Maulana Malik Ibrahim Malang. <a href="#" class="underline">All rights</a> reserved.
         </p>
